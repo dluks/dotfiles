71b0404d4d70a8fb4ae330ae4428687fcd0f8fd4
Daniel Lusk <daniel.lusk@posteo.net>
2024-07-01 08:57:57 +0000

    fix: run p10k prompt to fix instant prompt warning

--- setup.sh
+++ setup.sh
@@ -1,80 +1,79 @@
 #!/bin/bash
 
 show_help() {
-  echo "Usage: $0 [-b]"
-  echo "  -b    Create backup of existing package git directories"
+	echo "Usage: $0 [-b]"
+	echo "  -b    Create backup of existing package git directories"
 }
 
 create_backup=false
 
 while getopts "bh" opt; do
-  case ${opt} in
-    o )
-      create_backup=true
-      ;;
-    h )
-      show_help
-      exit 0
-      ;;
-    \? )
-      echo "Invalid option: $OPTARG" 1>&2
-      show_help
-      exit 1
-      ;;
-  esac
+	case ${opt} in
+	o)
+		create_backup=true
+		;;
+	h)
+		show_help
+		exit 0
+		;;
+	\?)
+		echo "Invalid option: $OPTARG" 1>&2
+		show_help
+		exit 1
+		;;
+	esac
 done
 
 # Install Tmux Plugin Manager (TPM)
 tpm_dir="$HOME/.tmux/plugins/tpm"
 if [ -d "$tpm_dir" ]; then
-  if $create_backup; then
-    echo "Found existing TPM directory. Creating backup and overwriting..."
-    mv "$tpm_dir" "$HOME/.tmux/plugins/tpm.bak"
-  else
-    echo "Found existing TPM directory. Removing without backup..."
-  fi
-  rm -rf "$tpm_dir"
+	if $create_backup; then
+		echo "Found existing TPM directory. Creating backup and overwriting..."
+		mv "$tpm_dir" "$HOME/.tmux/plugins/tpm.bak"
+	else
+		echo "Found existing TPM directory. Removing without backup..."
+	fi
+	rm -rf "$tpm_dir"
 fi
 git clone https://github.com/tmux-plugins/tpm "$tpm_dir"
 
 # Install LazyVIM
 nvim_dir="$HOME/.config/nvim"
 if [ -d "$nvim_dir" ]; then
-  if $create_backup; then
-    echo "Found existing nvim directory. Creating backup and overwriting..."
-    mv "$nvim_dir" "$HOME/.config/nvim.bak"
-  else
-    echo "Found existing nvim directory. Removing without backup..."
-  fi
-  rm -rf "$nvim_dir"
+	if $create_backup; then
+		echo "Found existing nvim directory. Creating backup and overwriting..."
+		mv "$nvim_dir" "$HOME/.config/nvim.bak"
+	else
+		echo "Found existing nvim directory. Removing without backup..."
+	fi
+	rm -rf "$nvim_dir"
 fi
 git clone https://github.com/LazyVim/starter "$nvim_dir"
 
 # Install Oh My Zsh
 ohmyzsh_dir="$HOME/.oh-my-zsh"
 if [ -d "$ohmyzsh_dir" ]; then
-  if $create_backup; then
-	echo "Found existing Oh My Zsh directory. Creating backup and overwriting..."
-	mv "$ohmyzsh_dir" "$HOME/.oh-my-zsh.bak"
-  else
-	echo "Found existing Oh My Zsh directory. Removing without backup..."
-  fi
-  rm -rf "$ohmyzsh_dir"
+	if $create_backup; then
+		echo "Found existing Oh My Zsh directory. Creating backup and overwriting..."
+		mv "$ohmyzsh_dir" "$HOME/.oh-my-zsh.bak"
+	else
+		echo "Found existing Oh My Zsh directory. Removing without backup..."
+	fi
+	rm -rf "$ohmyzsh_dir"
 fi
 
 RUNZSH=no zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-
 # Install Zsh Autosuggestions
 zsh_autosuggestions_dir="$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 if [ -d "$zsh_autosuggestions_dir" ]; then
-  if $create_backup; then
-	echo "Found existing Zsh Autosuggestions directory. Creating backup and overwriting..."
-	mv "$zsh_autosuggestions_dir" "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions.bak"
-  else
-	echo "Found existing Zsh Autosuggestions directory. Removing without backup..."
-  fi
-  rm -rf "$zsh_autosuggestions_dir"
+	if $create_backup; then
+		echo "Found existing Zsh Autosuggestions directory. Creating backup and overwriting..."
+		mv "$zsh_autosuggestions_dir" "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions.bak"
+	else
+		echo "Found existing Zsh Autosuggestions directory. Removing without backup..."
+	fi
+	rm -rf "$zsh_autosuggestions_dir"
 fi
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -82,13 +81,13 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # Install Zsh Syntax Highlighting
 zsh_syntax_highlighting_dir="$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 if [ -d "$zsh_syntax_highlighting_dir" ]; then
-  if $create_backup; then
-	echo "Found existing Zsh Syntax Highlighting directory. Creating backup and overwriting..."
-	mv "$zsh_syntax_highlighting_dir" "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting.bak"
-  else
-	echo "Found existing Zsh Syntax Highlighting directory. Removing without backup..."
-  fi
-  rm -rf "$zsh_syntax_highlighting_dir"
+	if $create_backup; then
+		echo "Found existing Zsh Syntax Highlighting directory. Creating backup and overwriting..."
+		mv "$zsh_syntax_highlighting_dir" "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting.bak"
+	else
+		echo "Found existing Zsh Syntax Highlighting directory. Removing without backup..."
+	fi
+	rm -rf "$zsh_syntax_highlighting_dir"
 fi
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -97,7 +96,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 curl -o /tmp/Meslo.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip
 # Check if .local/share/fonts exists
 if [ ! -d "$HOME/.local/share/fonts" ]; then
-  mkdir -p "$HOME/.local/share/fonts"
+	mkdir -p "$HOME/.local/share/fonts"
 fi
 unzip /tmp/Meslo.zip -d "$HOME/.local/share/fonts"
 fc-cache -f -v
@@ -108,27 +107,33 @@ sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/
 
 # Remove existing .zshrc
 if [ -f "$HOME/.zshrc" ]; then
-	  if $create_backup; then
-		      echo "Found existing .zshrc. Creating backup and overwriting..."
-		          mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
-			    else
-				        echo "Found existing .zshrc. Removing without backup..."
-					  fi
-					    rm -f "$HOME/.zshrc"
+	if $create_backup; then
+		echo "Found existing .zshrc. Creating backup and overwriting..."
+		mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
+	else
+		echo "Found existing .zshrc. Removing without backup..."
+	fi
+	rm -f "$HOME/.zshrc"
 fi
 # Remove existing .tmux.conf
 if [ -f "$HOME/.tmux.conf" ]; then
-	  if $create_backup; then
-		      echo "Found existing .tmux.conf. Creating backup and overwriting..."
-		          mv "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak"
-			    else
-				        echo "Found existing .tmux.conf. Removing without backup..."
-					  fi
-					    rm -f "$HOME/.tmux.conf"
+	if $create_backup; then
+		echo "Found existing .tmux.conf. Creating backup and overwriting..."
+		mv "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak"
+	else
+		echo "Found existing .tmux.conf. Removing without backup..."
+	fi
+	rm -f "$HOME/.tmux.conf"
 fi
 
 # Stow dotfiles
 stow .
 
 # Source stowed .zshrc
+source ~/.zshrc
+
+# Run powerlevel10k configuration wizard
+p10k configure
+
+# Source .zshrc again to register p10k configuration
 source ~/.zshrc
